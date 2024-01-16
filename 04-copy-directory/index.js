d1903e6af59d9f6e0d7a71f38562f42067b73c64
Koshman-Dmitri <123456desher@rambler.ru>
2024-01-16 15:02:24 +0300

    feat: solve task 04-copy-directory

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+const sourcePath = path.join(__dirname, 'files');
+const destPath = path.join(__dirname, 'files-copy');
+
+function copyDir() {
+  fs.mkdir(destPath, {recursive: true}, () => {});
+  fs.readdir(sourcePath, (err, files) => {
+    files.forEach((file) => {
+      const filePath = path.join(sourcePath, file);
+      const copyFilePath = `${destPath}/${file}`;
+      fs.copyFile(filePath, copyFilePath, () => {});
+    });
+  });
+}
+copyDir();
