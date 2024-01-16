3ef255b023a6ba6bda5c2856006be54643683d82
Koshman-Dmitri <123456desher@rambler.ru>
2024-01-16 14:59:44 +0300

    feat: solve task 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = process;
+
+const myPath = path.join(__dirname, 'output.txt');
+
+function writeData(data) {
+  fs.writeFile(myPath, data, {flag: 'a'}, () => {});
+}
+
+writeData('');
+
+stdout.write('Enter text\n');
+stdin.on('data', (data) => {
+  const strData = data.toString().slice(0, -2);
+  if (strData === 'exit') process.exit();
+  writeData(data);
+});
+
+process.on('exit', () => stdout.write('Goodbye!'));
+process.on('SIGINT', () => process.exit());
