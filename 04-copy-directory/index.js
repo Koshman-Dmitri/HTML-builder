42813191626e67b50003ce3688b6096e6d6e16fb
Koshman-Dmitri <123456desher@rambler.ru>
2024-01-20 13:18:58 +0300

    fix: task 04 updating dir when removing file

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,6 +6,12 @@ const destPath = path.join(__dirname, 'files-copy');
 
 function copyDir() {
   fs.mkdir(destPath, { recursive: true }, () => {});
+  fs.readdir(destPath, (err, files) => {
+    files.forEach((file) => {
+      const filePath = path.join(destPath, file);
+      fs.unlink(filePath, () => {});
+    });
+  });
   fs.readdir(sourcePath, (err, files) => {
     files.forEach((file) => {
       const filePath = path.join(sourcePath, file);
